9b10c58cd55558b0a08f49b75af9d7e9c156b0a7
松崎良太 <matsuzakiryouta@matsuzakiryoutanoMacBook-Pro.local>
2017-06-14 23:22:08 +0900

    update

--- book_introduction/db/schema.rb
+++ book_introduction/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612111939) do
+ActiveRecord::Schema.define(version: 20170614082409) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20170612111939) do
     t.integer  "rating"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "book_id"
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_comments_on_book_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
 end
